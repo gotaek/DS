0d9d48eb2162f212bb60be714561b76148a31541
deriko <deriko@naver.com>
2020-08-24 20:22:21 +0900

    binary insertion sort

--- insertionSort.py
+++ insertionSort.py
@@ -1,25 +1,40 @@
 from typing import MutableSequence
 
-def insertion_sort(a:MutableSequence)->None:
+def binary_insertion_sort(a:MutableSequence)->None:
 
     n=len(a)
     for i in range(1,n):
-        j=i
-        tmp=a[i]
-        while j>0 and a[j-1]>tmp:
+        key=a[i]
+        pl=0
+        pr=i-1
+
+        while True:
+            pc=(pl+pr)//2
+
+            if a[pc]==key:
+                break
+            elif a[pc]<key:
+                pl=pc+1
+            else:
+                pr=pc-1
+            if pl>pr:
+                break
+
+        pd=pc+1 if pl<= pr else pr +1
+
+        for j in range(i,pd,-1):
             a[j]=a[j-1]
-            j-=1
-        a[j]=tmp
+        a[pd]=key
 
 if __name__=='__main__':
-    print('단순 삽입 정렬을 수행합니다.')
+    print('이진 삽입 정렬을 수행합니다.')
     num=int(input('원소 수를 입력하세요.:'))
     x=[None]*num
 
     for i in range(num):
         x[i]=int(input(f'x[{i}]: '))
 
-    insertion_sort(x)
+    binary_insertion_sort(x)
 
     print('오름차순으로 정렬했습니다.')
     for i in range(num):
