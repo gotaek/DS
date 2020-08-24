d1397834e9d0d17dcbef803b09921f6bbba60232
deriko <deriko@naver.com>
2020-08-24 18:39:48 +0900

    bubble sort 3

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,14 +2,15 @@ from typing import MutableSequence
 
 def bubble_sort(a:MutableSequence)->None:
     n=len(a)
-    for i in range(n-1):
-        exchng=0
-        for j in range (n-1,i,-1):
+    k=0
+    while k<n-1:
+        last=n-1
+        
+        for j in range (n-1,k,-1):
             if a[j-1]>a[j]:
                 a[j-1],a[j]=a[j],a[j-1]
-            exchng+=1
-        if exchng==0:
-            break
+                last=j
+        k=last
 
 if __name__=='__main__':
     print('버블 정렬을 수행합니다.')
